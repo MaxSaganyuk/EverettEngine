e51af935a489cd3a02ddee11d90986ab65f52bc5
MaxSaganyuk <maxsaganyuk@gmail.com>
2025-04-03 19:26:01 +0300

    Fixed inability to resolve non base interface for camera macro

--- ScriptTest/EverettScript.h
+++ ScriptTest/EverettScript.h
@@ -5,15 +5,15 @@
 #include "ILightSim.h"
 #include "ISoundSim.h"
 
-#define CameraScriptLoop()                                                \
-void ImplCamera(ICameraSim& camera);                                      \
-                                                                          \
-extern "C" __declspec(dllexport) void Camera(void* camera)                \
-{                                                                         \
-    ImplCamera(*reinterpret_cast<ICameraSim*>(camera));                   \
-}                                                                         \
-                                                                          \
-void ImplCamera(ICameraSim& camera)                                       \
+#define CameraScriptLoop()                                                        \
+void ImplCamera(ICameraSim& camera);                                              \
+                                                                                  \
+extern "C" __declspec(dllexport) void Camera(void* camera)                        \
+{                                                                                 \
+    ImplCamera(*dynamic_cast<ICameraSim*>(reinterpret_cast<ISolidSim*>(camera))); \
+}                                                                                 \
+                                                                                  \
+void ImplCamera(ICameraSim& camera)                                               \
 
 #define ScriptObjectInit(name, objectType)                                        \
 void Impl##name(objectType& object##name);                                        \
